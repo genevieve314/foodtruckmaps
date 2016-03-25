ef64d7e3ca32ba855cc0d1bf2c6fb8a0661dd77b
Genevieve <gkolve@gmail.com>
2016-03-25 16:43:47 -0700

    [feature] queries city data api

--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
 var express = require('express');
-
 var app = express();
+var request = require('request');
+
 
 app.use(express.static(__dirname + '/../client/'));
 
 app.listen(3000);
+
+var host = 'https://data.sfgov.org/resource/rqzj-sfat.json';
+
+request.get(host, function(err, res, body){
+	console.log('res body in app.post to api: ', body);
+})
+
